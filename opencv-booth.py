6fb7a4bd4df162fbab6943322a43b4bec87a8df3
Josh Chisholm <joshuachisholm@gmail.com>
2019-10-19 20:26:35 +0100

    Super hack

--- opencv-booth.py
+++ opencv-booth.py
@@ -19,10 +19,18 @@ windowName = 'Photo Booth'
 state = 'idle'
 slideshowStartedAt = None
 
+windowWidth = 800
+windowHeight = 600
+
+cam = cv2.VideoCapture(0)
+
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, windowWidth)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, windowHeight)
+
 # cv2.namedWindow(windowName, cv2.WND_PROP_FULLSCREEN)
 # cv2.setWindowProperty(windowName,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
-cam = cv2.VideoCapture(0)
+
 
 def photoBooth():
     global state
@@ -31,7 +39,7 @@ def photoBooth():
         if state == 'idle':
             showImage('images/press-the-button.png')
 
-        if cv2.waitKey(1) == 32: # button pressed
+        if cv2.waitKey(1) == 32: # button pressedwindowHeght
             buttonPressed()
 
         if state == 'slideshow':
@@ -58,6 +66,9 @@ def startSlideshow():
     if not os.path.exists('tmp'):
         os.mkdir('tmp')
 
+    if not os.path.exists('photos'):
+        os.mkdir('photos')
+
     removeImage('tmp/photo-1.jpg')
     removeImage('tmp/photo-2.jpg')
     removeImage('tmp/photo-3.jpg')
@@ -75,27 +86,41 @@ def continueSlideshow():
     global previousMs
 
     ms = currentTimeMilliseconds() - slideshowStartedAt
-    if 1000 <= ms < 2000:
-        showImage('images/photo-1.png')
-    elif 2000 <= ms < 3000:
-        showImage('images/countdown-3.png')
-    elif 3000 <= ms < 4000:
-        showImage('images/countdown-2.png')
-    elif 4000 <= ms < 5000:
-        showImage('images/countdown-1.png')
-    elif 5000 <= ms < 6000:
-        showImage('images/cheese.png')
-    elif 6000 <= ms < 7000:
-        showCamera()
-    elif ms > 7000 and previousMs < 7000:
-        takePicture(1)
-    elif 7000 <= ms < 8000:
-        showPhotoSheet()
-    elif 8000 <= ms < 9000:
+    if ms < 10000:
+        continueSlideshowStep(1, ms, previousMs)
+    elif ms < 20000:
+        continueSlideshowStep(2, ms - 10000, previousMs - 10000)
+    elif ms < 30000:
+        continueSlideshowStep(3, ms - 20000, previousMs - 20000)
+    elif ms < 40000:
+        continueSlideshowStep(4, ms - 30000, previousMs - 30000)
+    elif ms > 40000 and previousMs < 40000:
+        savePhotoSheet()
+    elif ms > 50000:
         state = 'idle'
 
     previousMs = ms
-    
+
+def continueSlideshowStep(step, ms, previousMs):
+    if ms < 1000:
+        showImage('images/photo-' + str(step) + '.png')
+    elif ms < 2000:
+        showImage('images/countdown-3.png')
+    elif ms < 3000:
+        showImage('images/countdown-2.png')
+    elif ms < 4000:
+        showImage('images/countdown-1.png')
+    elif ms < 4500:
+        showImage('images/cheese.png')
+    elif ms < 7500:
+        showCamera()
+    elif ms >= 7500 and previousMs < 7500:
+        takePhoto(step)
+    elif ms < 8000:
+        showPhoto(step)
+    elif ms < 9000:
+        showPhotoSheet()
+
 def showImage(filename):
     img = cv2.imread(filename, 1)
     cv2.imshow(windowName, img)
@@ -103,16 +128,41 @@ def showImage(filename):
 def showCamera():
     _, img = cam.read()
     # if mirror: 
-    #     img = cv2.flip(img, 1)
+    img = cv2.flip(img, 1)
     cv2.imshow(windowName, img)
 
-def takePicture(number):
+def takePhoto(number):
     _, img = cam.read()
+    img = cv2.flip(img, 1)
     cv2.imwrite('tmp/photo-' + str(number) + '.jpg', img)
 
 def showPhotoSheet():
-    img = cv2.imread('tmp/photo-1.jpg', 1)
-    cv2.imshow(windowName, img)
+    photoSheetTop = np.concatenate((photoSheetImage(1), photoSheetImage(2)), axis=1)
+    photoSheetBot = np.concatenate((photoSheetImage(3), photoSheetImage(4)), axis=1)
+    photoSheet = np.concatenate((photoSheetTop, photoSheetBot), axis=0)
+    cv2.imshow(windowName, photoSheet)
+
+def savePhotoSheet():
+    photoSheetTop = np.concatenate((readPhoto(1), readPhoto(2)), axis=1)
+    photoSheetBot = np.concatenate((readPhoto(3), readPhoto(4)), axis=1)
+    photoSheet = np.concatenate((photoSheetTop, photoSheetBot), axis=0)
+    cv2.imwrite('photos/photo-' + str(currentTimeMilliseconds()) + '.jpg', photoSheet)
+
+def photoSheetImage(number):
+    image = None
+    if os.path.exists('tmp/photo-' + str(number) + '.jpg'):
+        image = cv2.imread('tmp/photo-' + str(number) + '.jpg', 1)
+    else:
+        image = cv2.imread('images/4up-background.png', 1)
+
+    return cv2.resize(image, (int(windowWidth / 2), int(windowHeight / 2)))
+
+def readPhoto(number):
+    return cv2.imread('tmp/photo-' + str(number) + '.jpg', 1)
+
+def showPhoto(number):
+    img = readPhoto(number)
+    cv2.imshow(windowName, cv2.resize(img, (windowWidth, windowHeight)))
 
 def currentTimeMilliseconds():
     return int(round(time.time() * 1000))
